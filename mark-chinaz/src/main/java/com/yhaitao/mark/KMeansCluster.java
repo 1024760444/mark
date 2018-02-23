74c910ed2ff904494c5701126a8788b733d25186
yanghaitao <1024760444@qq.com>
2018-02-23 21:46:54 +0800

    kmeans算法初步实现。

---
+++ mark-chinaz/src/main/java/com/yhaitao/mark/KMeansCluster.java
@@ -0,0 +1,190 @@
+package com.yhaitao.mark;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.cli.BasicParser;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.LocatedFileStatus;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.RemoteIterator;
+import org.apache.hadoop.io.SequenceFile;
+import org.apache.hadoop.io.SequenceFile.Writer;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.MRJobConfig;
+import org.apache.hadoop.util.StringUtils;
+import org.apache.hadoop.util.ToolRunner;
+import org.apache.mahout.vectorizer.SparseVectorsFromSequenceFiles;
+
+/**
+ * Kmean聚类实现。
+ * @author yhaitao
+ *
+ */
+public class KMeansCluster {
+	public static void main(String[] args) throws Exception {
+		// 参数解析，获取参数
+		CommandLine commands = getCommandLine(args);
+		if(commands == null) {
+			return ;
+		}
+		String original = commands.getOptionValue("original");
+		String in = commands.getOptionValue("in");
+		String out = commands.getOptionValue("out");
+		String extJars = commands.getOptionValue("extJars");
+		
+		// 参数设置
+		Configuration conf = new Configuration();
+		URI[] uriArray = getExtJars(conf, extJars);
+		String sfiles = StringUtils.uriToString(uriArray);
+		conf.set(MRJobConfig.CACHE_FILES, sfiles);
+		
+		// 数据写入HDFS
+		writeToSequenceFile(conf, original, in);
+		
+		// 生成向量
+		sequenceToSparse(conf, in, out);
+	}
+	
+	/**
+	 * 生成向量
+	 * @param in
+	 * @param out
+	 * @throws Exception 
+	 */
+	private static void sequenceToSparse(Configuration conf, String in, String out) throws Exception {
+		String[] args = {
+				"-i", in,
+				"-o", out,
+				"-lnorm", // 归一化
+				"-nv", 
+				"-ow", // 已有输出，覆盖
+				"-wt", "tfidf", // 计算权重
+				"-a", "org.wltea.analyzer.lucene.IKAnalyzer" // IK分词器
+		};
+		ToolRunner.run(conf, new SparseVectorsFromSequenceFiles(), args);
+	}
+	
+	/**
+	 * 获取第三方jar包的路径。
+	 * @param conf 集群配置
+	 * @param extJarsPath 第三方Jar包路径
+	 * @return 路径
+	 * @throws IOException
+	 */
+	private static URI[] getExtJars(Configuration conf, String extJarsPath) throws IOException {
+		// 读取hdfs文件，获取jar列表
+		FileSystem fileSystem = FileSystem.get(conf);
+		RemoteIterator<LocatedFileStatus> listFiles = fileSystem.listFiles(new Path(extJarsPath), false);
+		List<URI> uriList = new ArrayList<URI>();
+		while(listFiles.hasNext()) {
+			LocatedFileStatus next = listFiles.next();
+			uriList.add(next.getPath().toUri());
+		}
+		fileSystem.close();
+		URI[] array = new URI[uriList.size()];
+		return uriList.toArray(array);
+	}
+	
+	/**
+	 * 
+	 * @param original
+	 * @param in
+	 * @throws IOException 
+	 */
+	private static void writeToSequenceFile(Configuration conf, String original, String in) throws IOException {
+		// SequenceFile写入工具
+		Path path = new Path(in + "/original-seqdir/part-m-00000");
+		SequenceFile.Writer writer = SequenceFile.createWriter(
+				conf, 
+				new SequenceFile.Writer.Option[]{
+						SequenceFile.Writer.file(path),
+						Writer.keyClass(Text.class),
+						Writer.valueClass(Text.class)
+				});
+		
+		// 写入数据
+		Map<String, String> map = KMeansCluster.readOriginalFile(original);
+		for(String key : map.keySet()) {
+			String value = map.get(key);
+			writer.append(new Text(key), new Text(value));
+		}
+		writer.close();
+	}
+	
+	/**
+	 * 读取原始数据。
+	 * @param original 原始数据目录
+	 * @return 原始数据键值对列表
+	 * @throws IOException 
+	 */
+	private static Map<String, String> readOriginalFile(String original) throws IOException {
+		File file = new File(original);
+		BufferedReader br = new BufferedReader(new FileReader(file));
+		String s = null;
+		Map<String, String> data = new HashMap<String, String>();
+		while ((s = br.readLine()) != null) {
+			// 使用readLine方法，一次读一行
+			String[] split = s.split(" ");
+			if(split != null && split.length >= 2) {
+				data.put(split[0], split[1]);
+			}
+		}
+		br.close();
+		return data;
+	}
+
+	/**
+	 * 入参校验
+	 * @param args 入参
+	 * @throws ParseException 
+	 */
+	private static CommandLine getCommandLine(String[] args) throws ParseException {
+		Options options = buildOptions();
+		BasicParser parser = new BasicParser();
+		CommandLine commands = parser.parse(options, args);
+		if(!commands.hasOption("in")
+				|| !commands.hasOption("out")
+				|| !commands.hasOption("original")
+				|| !commands.hasOption("extJars")) {
+			printUsage(options);
+			return null;
+		} else {
+			return commands;
+		}
+	}
+	
+	/**
+	 * 输入信息提示。
+	 * @return 需要输入数据的说明
+	 */
+	private static Options buildOptions() {
+		Options options = new Options();
+		options.addOption("in", true, "[required] HDFS path for kmeans input");
+		options.addOption("out", true, "[required] HDFS path for kmeans out");
+		options.addOption("original", true, "[required] original file path");
+		options.addOption("extJars", true, "[required] HDFS path ext jars");
+		return options;
+	}
+	
+	/**
+	 * 打印输入信息。
+	 * @param options 打印输入帮助信息。
+	 */
+	public static void printUsage(Options options) {
+		HelpFormatter help = new HelpFormatter();
+		help.printHelp("Job of KMeansCluster need Params : ", options);
+	}
+}
--- mark-chinaz/src/main/java/com/yhaitao/mark/chinaz/ChinazCrawler.java
+++ mark-chinaz/src/main/java/com/yhaitao/mark/chinaz/ChinazCrawler.java
@@ -2,7 +2,11 @@ package com.yhaitao.mark.chinaz;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -13,12 +17,19 @@ import org.slf4j.LoggerFactory;
 import com.yhaitao.mark.chinaz.bean.ChinazWeb;
 import com.yhaitao.mark.http.MarkHttpClient;
 
+/**
+ * 运行命令： nohup java -jar mark-chinaz.jar /home/app/jobs/mark-chinaz/chinazWebInfo.txt & 
+ * @author Administrator
+ *
+ */
 public class ChinazCrawler {
 	/**
 	 * 日志对象
 	 */
 	private final static Logger LOGGER = LoggerFactory.getLogger(ChinazCrawler.class);
 	private static final MarkHttpClient httpClient = new MarkHttpClient();
+	private static final Map<String, String> DOMAINMAP = new HashMap<String, String>();
+	
 	public static void main(String[] args) throws Exception {
 		String baseUrl = "http://top.chinaz.com/hangye/";
 		for(int i = 1; i <= 1881; i++) {
@@ -27,20 +38,23 @@ public class ChinazCrawler {
 				String response = httpClient.httpGet(crawlerUrl);
 				List<ChinazWeb> filterList = filterList(response);
 				String outdata = outdata(filterList);
-				FileUtils.write(new File(args[0]), outdata, "UTF-8", true);
+				FileUtils.write(new File(args[0] + "/webdata.txt"), outdata, "UTF-8", true);
 				LOGGER.info("success crawlerUrl : " + crawlerUrl);
 			} catch (Exception e) {
 				try {
 					String response = httpClient.httpGet(crawlerUrl);
 					List<ChinazWeb> filterList = filterList(response);
 					String outdata = outdata(filterList);
-					FileUtils.write(new File(args[0]), outdata, "UTF-8", true);
+					FileUtils.write(new File(args[0] + "/webdata.txt"), outdata, "UTF-8", true);
 					LOGGER.info("success crawlerUrl : " + crawlerUrl);
 				} catch (Exception e1) {
 					LOGGER.error("error crawlerUrl : " + crawlerUrl + ", Exception : " + e.getMessage());
 				}
 			}
 		}
+		
+		// 记录域名
+		FileUtils.write(new File(args[0] + "/domainInfo.txt"), getDomainIdList(), "UTF-8", true);
 	}
 	
 	/**
@@ -48,15 +62,50 @@ public class ChinazCrawler {
 	 * @param filterList
 	 * @return
 	 */
-	public static String outdata(List<ChinazWeb> filterList) {
+	private static String outdata(List<ChinazWeb> filterList) {
 		StringBuffer outData = new StringBuffer();
 		for(ChinazWeb web : filterList) {
 			if(outData.length() > 0) {
 				outData.append("\n");
 			}
-			outData.append(web.getDomain()).append(" ").append(web.getName()).append(" ").append(web.getDesc().replace("网站简介：", ""));
+			outData.append(getDomainId(web.getDomain())).append(" ").append(web.getName()).append(" ").append(web.getDesc().replace("网站简介：", ""));
 		}
 		return outData.toString();
+	}
+	
+	/**
+	 * 根据域名，获取域名的编号。
+	 * @param domain 域名
+	 * @return 域名编号
+	 */
+	private static String getDomainId(String domain) {
+		String domainId = null;
+		if(DOMAINMAP.containsKey(domain)) {
+			domainId = DOMAINMAP.get(domain);
+		} else {
+			domainId = String.valueOf(1000000 + DOMAINMAP.size());
+			DOMAINMAP.put(domain, domainId);
+		}
+		return domainId;
+	}
+	
+	/**
+	 * 输出域名编号字符串。
+	 * @return 域名编号字符串，便于输出
+	 */
+	private static String getDomainIdList() {
+		StringBuffer domainIdList = new StringBuffer();
+		Iterator<Entry<String, String>> iterator = DOMAINMAP.entrySet().iterator();
+		while(iterator.hasNext()) {
+			Entry<String, String> next = iterator.next();
+			String key = next.getKey();
+			String value = next.getValue();
+			if(domainIdList.length() > 0) {
+				domainIdList.append("\n");
+			}
+			domainIdList.append(value).append(" ").append(key);
+		}
+		return domainIdList.toString();
 	}
 	
 	/**
@@ -64,7 +113,7 @@ public class ChinazCrawler {
 	 * @param context 网页源码
 	 * @return 网页的标题
 	 */
-	public static List<ChinazWeb> filterList(String context) {
+	private static List<ChinazWeb> filterList(String context) {
 		List<ChinazWeb> webList = new ArrayList<ChinazWeb>();
 		Pattern pa = Pattern.compile("<div class=\"CentTxt\">(.*?)<div class=\"RtCRateWrap\">");
 		Matcher ma = pa.matcher(context);
@@ -88,7 +137,7 @@ public class ChinazCrawler {
 	 * @param regex
 	 * @return
 	 */
-	public static String filter(String context, String regex) {
+	private static String filter(String context, String regex) {
 		Pattern pa = Pattern.compile(regex);
 		Matcher ma = pa.matcher(context);
         String title = null;
